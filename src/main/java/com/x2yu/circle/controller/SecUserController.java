1ae85792171fb77ddb23cc40d61146dbdb009a9a
xxysama <374439529@qq.com>
2020-03-06 21:44:52 +0800

    用户信息修改

--- src/main/java/com/x2yu/circle/controller/SecUserController.java
+++ src/main/java/com/x2yu/circle/controller/SecUserController.java
@@ -6,13 +6,12 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.x2yu.circle.entity.SecUser;
 import com.x2yu.circle.entity.TblUserLike;
 import com.x2yu.circle.service.ISecUserService;
+import com.x2yu.circle.utils.Result;
+import com.x2yu.circle.utils.ResultUtil;
+import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -40,6 +39,7 @@ public class SecUserController {
 
     @GetMapping("list/{page}")
     @ApiOperation("获取用户列表")
+    @ApiImplicitParam(name = "page",value = "页码",required = true,dataType = "Integer")
     public  IPage<SecUser> getUserListByPage(@PathVariable("page")Integer page){
 
         // 分页查询每页的数据量
@@ -49,5 +49,21 @@ public class SecUserController {
         IPage<SecUser> userIPage = userService.page(userPage);
 
         return userIPage;
+    }
+
+    @PutMapping("active")
+    @ApiOperation("改变用户状态")
+    public Result changeUserActive(@RequestBody SecUser user){
+        Integer uid = user.getUserId();
+        try {
+            user = userService.getById(uid);
+            user.setActive(!user.getActive());
+            userService.updateById(user);
+            return ResultUtil.success();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return ResultUtil.submitError();
+        }
+
     }
 }
